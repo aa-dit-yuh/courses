cb9da9864e0a1525866f5e308c77f842506d95d7
Aditya Narayan <narayanaditya95@gmail.com>
2015-05-19 11:53:54 +0530

    Completed Project-4

--- coursera/nand2tetris1/projects/04/mult/Mult.asm
+++ coursera/nand2tetris1/projects/04/mult/Mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Initialize sum to be zero
+    @sum
+    M = 0;
+
+(LOOP)
+// Load counter
+    @R0
+    D = M;
+
+// Check counter is zero
+    @END
+    D; JEQ
+
+    @R1
+    D = M;
+    @sum
+    M = M + D;
+
+//  Decrement counter
+    @R0
+    M = M - 1;
+
+    @LOOP
+    0; JMP
+
+(END)
+// Save @sum to @R2
+    @sum
+    D = M;
+    @R2
+    M = D;
+
+    @END
+    0; JMP
